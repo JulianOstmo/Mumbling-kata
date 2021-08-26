00110f84f9249978051c69bd05f6046749a44ab8
Julian Ostmo <julian.ostmo@ibm.com>
2021-08-26 11:27:33 +0100

    feat: mumble function complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-const temp = () => {
-  return true;
+const accum = (input) => {
+  if (typeof input !== 'string') {
+    throw new Error('Input must be a string');
+  }
+
+  return input
+    .split('')
+    .reduce((output, letter, index) => {
+      const trailingLetter = letter.toLowerCase().repeat(index);
+      const mumble = `${letter.toUpperCase()}${trailingLetter}`;
+      return [...output, mumble];
+    }, [])
+    .join('-');
 };
 
 module.exports = {
-  temp,
+  accum,
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,35 @@
-const { temp } = require('../src/index');
+const { accum } = require('../src/index');
 
-describe('Placeholder function', () => {
-  it('should return true', () => {
-    expect(temp()).toBe(true);
+describe('GIVEN the accum function', () => {
+  describe('WHEN an input that is not a string is passed', () => {
+    test('THEN throw an error', () => {
+      expect(() => {
+        accum(123);
+      }).toThrow();
+    });
+  });
+
+  describe('WHEN an empty string is passed', () => {
+    test('THEN return an empty string', () => {
+      expect(accum('')).toEqual('');
+    });
+  });
+
+  describe('WHEN the input is a string only containing letters', () => {
+    test('THEN an input of "a" should return "A"', () => {
+      expect(accum('a')).toEqual('A');
+    });
+
+    test('THEN an input of "ab" should return "A-Bb"', () => {
+      expect(accum('ab')).toEqual('A-Bb');
+    });
+
+    test('THEN an input of "ABC" should return "A-Bb-Ccc"', () => {
+      expect(accum('ABC')).toEqual('A-Bb-Ccc');
+    });
+
+    test('THEN an input of "RqaEzty" should return "A-Bb-Ccc"', () => {
+      expect(accum('RqaEzty')).toEqual('R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy');
+    });
   });
 });
